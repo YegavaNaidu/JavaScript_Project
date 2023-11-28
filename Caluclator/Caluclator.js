9ceabba0731d5ae077f41d9170fcfb72bec69e32
YegavaNaidu <yeagavanaidu@gmail.com>
2023-11-28 08:28:25 +0530

    caluclator

--- Caluclator/Caluclator.js
+++ Caluclator/Caluclator.js
@@ -1,1 +1,27 @@
-console.log('hi');
+input = document.getElementById('inputbox');
+buttons = document.querySelectorAll('button');
+
+
+string ="";
+arr = Array.from(buttons);
+console.log(arr);
+arr.forEach(button => {
+    button.addEventListener('click',(e)=>{
+        if(e.target.innerHTML == '='){
+            string = eval(string);
+            input.value = string;
+        }
+        else if(e.target.innerHTML == 'AC'){
+                string ="";
+                input.value =string;
+        }
+        else if (e.target.innerHTML == 'DEL'){
+            string = string.substring(0,string.length -1);
+            input.value =string;
+        }
+        else{
+        string += e.target.innerHTML;
+        input.value = string;
+        }
+    })
+});
