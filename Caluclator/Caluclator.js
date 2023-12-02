ed434feac7edf17233fb3e32329f055ad8ad0c9c
YegavaNaidu <yeagavanaidu@gmail.com>
2023-12-02 17:21:24 +0530

    added some conditions

--- Caluclator/Caluclator.js
+++ Caluclator/Caluclator.js
@@ -1,12 +1,30 @@
-input = document.getElementById('inputbox');
-buttons = document.querySelectorAll('button');
+let input = document.getElementById('inputbox');
+let buttons = document.querySelectorAll('button');
 
 
-string ="";
-arr = Array.from(buttons);
-console.log(arr);
+let string ="";
+let arr = Array.from(buttons);
+
+addEventListener("input",(e)=>{
+    string+=e.data;
+    console.log(string);
+})
+addEventListener("keydown",e =>{
+        if(e.key =='Enter'){
+            string = eval(string);
+            input.value = string;
+        }
+        if(e.key =='Backspace'){
+            if(string.length >1){
+            string = string.substring(0,string.length-1);
+            input.value =string;
+            }
+        }
+})
+
 arr.forEach(button => {
     button.addEventListener('click',(e)=>{
+        if(e.pointerType!== ""){
         if(e.target.innerHTML == '='){
             string = eval(string);
             input.value = string;
@@ -16,12 +34,13 @@ arr.forEach(button => {
                 input.value =string;
         }
         else if (e.target.innerHTML == 'DEL'){
-            string = string.substring(0,string.length -1);
+            string = string.substring(0,string.length-1);
             input.value =string;
         }
         else{
         string += e.target.innerHTML;
         input.value = string;
+        }
         }
     })
 });
