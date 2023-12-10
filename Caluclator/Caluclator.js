5c499409c47bfd726adc770e1dcefb1e32ec73dc
YegavaNaidu <yeagavanaidu@gmail.com>
2023-12-10 12:57:43 +0530

    modified

--- Caluclator/Caluclator.js
+++ Caluclator/Caluclator.js
@@ -1,29 +1,44 @@
-let input = document.getElementById('inputbox');
-let buttons = document.querySelectorAll('button');
+input = document.getElementById('inputbox');
+buttons = document.querySelectorAll('button');
 
 
-let string ="";
-let arr = Array.from(buttons);
+string ="";
+arr = Array.from(buttons);
 
-addEventListener("input",(e)=>{
-    string+=e.data;
+input.addEventListener("keydown",(e)=>{
+    if(e.code.substring(0,4) ==='Digit' || e.key =='*' || e.key =='+'|| e.key =='-'
+    || e.key =='/'|| e.key =='.'){
     console.log(string);
-})
-addEventListener("keydown",e =>{
-        if(e.key =='Enter'){
-            string = eval(string);
+    string += e.key;
+    }
+    else if(e.key =='Enter'){
+        string = eval(string);
+        input.value = string;
+    }
+    else if(e.key =='Backspace'){
+        if(string.length >0){
+                string = string.substring(0,string.length-1);
             input.value = string;
         }
-        if(e.key =='Backspace'){
-            if(string.length >1){
-            string = string.substring(0,string.length-1);
-            input.value =string;
-            }
-        }
+    }
+
 })
+// addEventListener("keydown",(e) =>{
+//         if(e.key =='Enter'){
+//             string = eval(string);
+//             input.value = string;
+//         }
+//         if(e.key =='Backspace'){
+//             if(string.length >0){
+//             string = string.substring(0,string.length-1);
+//             input.value = string;
+//             }
+//         }
+// })
 
 arr.forEach(button => {
     button.addEventListener('click',(e)=>{
+        console.log(string);
         if(e.pointerType!== ""){
         if(e.target.innerHTML == '='){
             string = eval(string);
